8615d92f7eb189c5491b90752fa09d3c0ca270e2
Collin OConnell <ococncol@gmail.com>
2017-06-22 16:31:14 -0500

    BookActivity to display all the books and to refresh/destroy them if there is a new search

--- app/src/main/java/design/shortnd/searchbooks/BookActivity.java
+++ app/src/main/java/design/shortnd/searchbooks/BookActivity.java
@@ -1,18 +1,31 @@
 package design.shortnd.searchbooks;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BookActivity extends AppCompatActivity {
+
+public class BookActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
 
     // String that will be put in to Log if error occurs
-    private static final String LOG_TAG = BookActivity.class.getName();
+    // private static final String LOG_TAG = BookActivity.class.getName();
 
     /**
      * URL for book data from the Google Book API
@@ -62,9 +75,110 @@ public class BookActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 // Find the current book that was clicked on
                 Book currentBook = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri bookUri = Uri.parse(currentBook.getBookUrl());
+
+                // Create a new Intent to view the Books URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, bookUri);
+
+                // Send the Intent to launch a new activity
+                startActivity(websiteIntent);
             }
         });
 
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with the loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(BOOK_LOADER_ID, null, this);
+        } else {
+            // Otherwise display error
+            // First, Hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateView.setText(R.string.no_internet_connection);
+        }
+
+    }
+
+    /**
+     * This is for the settings activity/menu to seach for the books
+     * (as of right now)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
+        // Gets the current settings and then querys the google book API
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Gets the value of the book
+        String book = sharedPrefs.getString(
+                getString(R.string.settings_book_key),
+                getString(R.string.settings_book_value)
+        );
+
+        Uri baseUri = Uri.parse(BOOK_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("q", book);
+
+        return new BookLoader(this, uriBuilder.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No Books found."
+        mEmptyStateView.setText(R.string.no_books);
+
+        // Clear the adpater of previous book data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Book}s, then add them to the adapter's
+        // data set. This will trigger the ListView update
+        if (books != null && !books.isEmpty()) {
+            mAdapter.addAll(books);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }

b76e7504cdc128d9454be83a17f2d5d8b397c355
Collin OConnell <ococncol@gmail.com>
2017-06-22 17:32:28 -0500

    Got settings activity to work able to change the value in the search preference..

--- app/src/main/java/design/shortnd/searchbooks/SettingsActivity.java
+++ app/src/main/java/design/shortnd/searchbooks/SettingsActivity.java
@@ -1,6 +1,54 @@
 package design.shortnd.searchbooks;
 
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class BookPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference book = findPreference(getString(R.string.settings_book_key));
+            bindPreferenceSummaryToValue(book);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+            if (o instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        public void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+    }
 }
